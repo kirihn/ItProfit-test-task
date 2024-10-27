a3bcd1be439fe5b347e6a401855d9aed611bc2eb
Zukay Kiryl <kzykov03@mail.ru>
2024-10-27 20:57:49 +0300

    feat: add webpack config

--- webpack-config.js
+++ webpack-config.js
@@ -1,9 +1,35 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
     entry: './src/js/index.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
-    }
-}
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'style-loader',
+                    'css-loader', 
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        open: true,
+    },
+};
